48053ca5f7221c6727824f84001c4191f59e4c28
Felipe Pessoa <felipessoaf@gmail.com>
2017-11-04 22:00:42 -0200

    cave

---
+++ Wumpus/Assets/Scripts/DisableLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableLight : MonoBehaviour {
+
+    private GameObject[] Lights;
+
+    private void Awake()
+    {
+        Lights = GameObject.FindGameObjectsWithTag("Light");
+    }
+
+    void OnPreCull()
+    {
+        foreach (GameObject light in Lights)
+        {
+            light.GetComponent<Light>().enabled = false;
+        }
+    }
+
+    void OnPostRender()
+    {
+        foreach (GameObject light in Lights)
+        {
+            light.GetComponent<Light>().enabled = true;
+        }
+    }
+}
