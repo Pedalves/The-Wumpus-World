c31362339af583fe1f344a1f50d23e1a8ec52e7a
fvbizzo <felipe.bizzo@gmail.com>
2017-11-13 15:58:02 -0200

    movimentaçao basica
    inicializa o agente e pode mover nas quatro direçoes

--- wumpus.pl
+++ wumpus.pl
@@ -1,7 +1,45 @@
-Here(X,Y).
-lugar_prox([X,Y], L, [New_X, Y]) :- New_X is X+1.
-lugar_prox([X,Y], N, [X, New_Y]) :- New_Y is Y+1.
-lugar_prox([X,Y], O, [New_X, Y]) :- New_x is X-1.
-lugar_prox([X,Y], S, [X, New_Y]) :- New_Y is Y+1.
-Location(col, lin).
-Adjacente(X1,X2) :- lugar_prox(X1,_,X2).
+start_pos(1/1).
+
+:- dynamic([agent_location/1]).
+
+init_agent :-
+    retractall(agent_location(_)),
+    assert(agent_location([1,1])).
+    
+show :- 
+    agent_location([X,Y]),
+    format("coluna ~p e linha ~p", [X,Y]).
+
+update_agent_location([X1,Y1]) :-
+    agent_location([X,Y]),
+    retractall( agent_location(_) ),
+    assert( agent_location([X1,Y1]) ),
+    format("Estou na coluna ~p e na linha ~p", [X1, Y1]).
+
+lugar_prox([X,Y], 0, [X2, Y]) :- X2 is X+1.
+lugar_prox([X,Y], 1, [X, Y2]) :- Y2 is Y+1.
+lugar_prox([X,Y], 2, [X2, Y]) :- X2 is X-1.
+lugar_prox([X,Y], 3, [X, Y2]) :- Y2 is Y+1.
+
+move_up :-
+    agent_location([X,Y]),
+    Y1 is Y+1,
+    update_agent_location([X,Y1]).
+
+move_down :-
+    agent_location([X,Y]),
+    Y1 is Y-1,
+    update_agent_location([X,Y1]).
+
+move_left :-
+    agent_location([X,Y]),
+    X1 is X-1,
+    update_agent_location([X1,Y]).
+
+move_right :-
+    agent_location([X,Y]),
+    X1 is X+1,
+    update_agent_location([X1,Y]).
+
+%Adjacente(X1,X2) :- lugar_prox(X1,0,X2).
+
